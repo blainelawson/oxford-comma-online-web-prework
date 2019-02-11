8efb1f90010419925eea9272e8f8aef34b4c10ce
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-11 22:15:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,7 @@ def oxford_comma(array)
   return array.join(" and ") if array.length == 2
   
   if array.length >= 2
-    last_in_array = array.pop
-    last_in_array = "and " + last_in_array
-    array.push(last_in_array)
+    array[-1].prepend("and ")
     array.join(", ")
   end
 end
