b79ad0f0d244644a3e8de04498083fa17e8f8433
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-11 22:01:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-
+  return array.join if array.length == 1
+  
+  return array.join(" and ") if array.length == 2
+  
+  return array.join(", and ") if array.length >= 2
 end
